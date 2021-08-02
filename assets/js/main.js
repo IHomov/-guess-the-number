afc412e455627125e1c0ffbfcf1cbaaef38b4940
IHomov <innapariy@gmail.com>
2021-08-02 16:06:55 +0300

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,26 @@
 
 let RandomNumber = Math.floor((Math.random() * 100) + 1);
 console.log(RandomNumber);
-    const max = 10;
-    
-    let CountTryies = 10; // будем высчитывать 10 - счетчик
-    let countGuess = 0; 
+    let CountTryies = 10; // количество  попыток
     let Text = "";
 
 for (let i = 0; i < 10; i++) {
-            countGuess = CountTryies;
-    
-            function guess() {
-                    let Uservariant = +inputNumber.value;
-                    if (Uservariant < RandomNumber) {
-                        Text = "меньше моего числа.";
-                        CountTryies--;
-                    }
-                    else if (Uservariant === RandomNumber) {
-                        Text = "Поздравляю, вы угадали!";
-                        
 
-                    }
-                    else {
-                        Text = " больше моего числа.";
-                        CountTryies--;
-                    }
-                                        
-                    resultPlace.innerHTML = (`${Uservariant} ${Text}`);
-                    numberOfattempts.innerHTML = (`${CountTryies}`);
+        function guess() {
+                let Uservariant = +inputNumber.value; //берем с поля input введенное число пользователя
+                if (Uservariant < RandomNumber) { 
+                    Text = "меньше моего числа.";
+                    CountTryies--;
                 }
-        
-    }
+                else if (Uservariant === RandomNumber) {
+                    Text = "Поздравляю, вы угадали!";
+                }
+                else {
+                    Text = " больше моего числа.";
+                    CountTryies--;
+                }
+                resultPlace.innerHTML = (`${Uservariant} ${Text}`);
+                numberOfattempts.innerHTML = (`${CountTryies}`);
+                }
+
+}
